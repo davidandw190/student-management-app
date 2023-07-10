0b51a3b3497ca563eb0882544c0cf3e61e52ad2a
Andrei-David Nan <nandavid190@gmail.com>
2023-07-10 22:49:55 +0300

    Configured `getStudents` method in app component;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Student } from './student';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { StudentService } from './student.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'student-management-app';
+
+  students!: Student[];
+  
+  constructor(private studentService: StudentService) {}
+
+  ngOnInit(): void {
+    this.getStudents();
+  }
+
+  public getStudents(): void {
+    this.studentService.getStudents()
+  };
+  
 }
+
+
